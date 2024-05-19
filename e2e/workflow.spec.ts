286d466259aabc5ebdd925fa7cafdc14cbe3dd98
Adil <adilarslan3@gmail.com>
2024-05-20 00:52:14 +0300

    Update tests to be more robust

--- e2e/workflow.spec.ts
+++ e2e/workflow.spec.ts
@@ -1,19 +1,25 @@
 import { test, expect } from '@playwright/test'
+import {en} from '../src/languages/en.js'
+import {cz} from '../src/languages/cz.js'
+import {lt} from '../src/languages/lt.js'
 
 test('language selection', async ({ page }) => {
   await page.goto('http://localhost:5173/')
-  await expect(page.getByTestId('title-homepage')).toContainText(/Scan the QR code to begin/i)
+  const english = en['home'].title
+  const czech = cz['home'].title
+  const lithuanian = lt['home'].title 
+
+  await expect(page.getByTestId('title-homepage')).toHaveText(english)
 
   await page.getByTestId('language-selection-menu-button').click()
   await page.getByTestId('language-option-cz').click()
-  await expect(page.getByTestId('title-homepage')).toContainText(/Naskenujte QR kód pro zahájení/i)
+  await expect(page.getByTestId('title-homepage')).toHaveText(czech)
 
   await page.getByTestId('language-selection-menu-button').click()
   await page.getByTestId('language-option-lt').click()
-  await expect(page.getByTestId('title-homepage')).toContainText(
-    /Skenuokite QR kodą, kad pradėtumėte/i
-  )
+  await expect(page.getByTestId('title-homepage')).toHaveText(lithuanian)
 })
+
 
 test('happy path without expects', async ({ page }) => {
   await page.goto('http://localhost:5173/')
@@ -32,17 +38,18 @@ test('happy path without expects', async ({ page }) => {
 })
 
 test('reseting to default language after each session', async ({ page }) => {
+  const english = en['home'].title
+  const lithuanian = lt['home'].title 
+
   await page.goto('http://localhost:5173/')
   await page.getByTestId('language-selection-menu-button').click()
   await page.getByTestId('language-option-lt').click()
-  await expect(page.getByTestId('title-homepage')).toContainText(
-    /Skenuokite QR kodą, kad pradėtumėte/i
-  )
+  await expect(page.getByTestId('title-homepage')).toHaveText(lithuanian)
 
   await page.getByTestId('generate-test-address-button').click()
   await page.getByTestId('continue-transaction-button-wallet').click()
   await page.getByTestId('continue-transaction-button-payment').click()
   await page.getByTestId('continue-transaction-button-review').click()
   await page.getByTestId('return-home-button').click()
-  await expect(page.getByTestId('title-homepage')).toContainText(/Scan the QR code to begin/i)
+  await expect(page.getByTestId('title-homepage')).toHaveText(english)
 })
